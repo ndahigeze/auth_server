7d7edb22bc47f433f128a1a9b20adfa3f85a7ec0
Ndahigeze Yesse Jean Lini <yessejeanlinindahigeze@gmail.com>
2021-03-31 12:02:31 +0200

    make changes

--- authserver/settings.py
+++ authserver/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_@19m$=h_1vyi8f1$^5mle&92k3@&phut7hge^*fhcqa0s1@qz'
+SECRET_KEY =env('AUTH_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
