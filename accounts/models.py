a8cb9acdb92c5e99e7712cdc51c71cff127c15de
ndahigeze <yessejeanlinindahigeze@gmail.com>
2021-12-16 16:36:14 +0200

    add layout

--- accounts/models.py
+++ accounts/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
+from encrypted_id.models import EncryptedIDModel
 
 from authserver.metadata import MetaData
 
 
-class User(AbstractUser,MetaData):
+class User(AbstractUser,MetaData,EncryptedIDModel):
     img = models.FileField(upload_to='static/media/', verbose_name='Change Profile Image')
     email = models.CharField(max_length=255, blank=False,null=False, unique=True)
     USERNAME_FIELD = "email"
